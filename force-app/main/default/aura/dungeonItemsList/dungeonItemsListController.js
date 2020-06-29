101b847da000a2236e6875c2c800ee39a3369e8f
heyitsaking <rey.arturo95@gmail.com>
2020-06-29 18:40:38 -0400

    Updating reference information component, adding community data

--- force-app/main/default/aura/dungeonItemsList/dungeonItemsListController.js
+++ force-app/main/default/aura/dungeonItemsList/dungeonItemsListController.js
@@ -1,6 +1,3 @@
 ({
-    handleSectionToggle: function(component, event, helper){
-        var items = component.get("v.items");
-        console.log(items);
-    }
+
 })
--- force-app/main/default/aura/dungeonItemsList/dungeonItemsListHelper.js
+++ force-app/main/default/aura/dungeonItemsList/dungeonItemsListHelper.js
@@ -1,14 +1,3 @@
 ({
-	setItems : function(component) {
-        var itemType = component.get("v.isWeapons");
-        var action = component.get("c.getCategoryItems");
-        var weaponTypes;
-        if(itemType) {
-            setTimeout(function() {
-                weaponTypes = component.get("v.items")
-            }, 1000);
-        } else {
-            
-        }
-	}
+
 })
